6fb531c6c56573b713e8b29891667f4c2e8dc54b
antonella-giovannetti <antonella.giovannetti@laplateforme.io>
2023-10-22 16:53:53 +0200

    add the 3 graph

--- beehive.py
+++ beehive.py
@@ -2,58 +2,56 @@ import random
 import numpy as np
 
 class Bee:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
         self.genes = []
-        self.hive = (500, 500)
-        self.fitness = 0
+        self.performance_score = 0
 
     def set_genes(self, flowers):
-        self.genes = [self.hive] + random.sample(flowers, len(flowers))
+        self.genes = random.sample(flowers, len(flowers))
 
-    def set_fitness(self):
-        current_flower = self.hive
-        distance_list = []
-        for gene in self.genes:
-            distance_list.append(self.distance(current_flower, gene))
-            current_flower = gene
-        self.fitness = sum(distance_list)
+    def calculate_performance(self):
+        # conversion en tableau numpy
+        genes_np = np.array(self.genes)
+        # calcul de la distance entre chaque fleur
+        distances = np.sum(np.abs(genes_np[:-1] - genes_np[1:]), axis=1)
+        # somme des distances
+        self.performance_score = np.sum(distances)
+        
+    def reproduce(self, parent1, parent2):
+        child1 = Bee()
+        child2 = Bee()
+        crossover_point = random.randint(1, len(self.genes) - 1)
+        child1_genes = parent1.genes[:crossover_point]
+        child2_genes = parent2.genes[:crossover_point]
+        missing_coords_child1 = [coord for coord in parent2.genes if coord not in child1_genes]
+        missing_coords_child2 = [coord for coord in parent1.genes if coord not in child2_genes]
+        child1_genes += missing_coords_child1
+        child2_genes += missing_coords_child2
+        child1.genes = child1_genes
+        child2.genes = child2_genes
+        return child1, child2
 
-    def distance(self, x, y):
-        return (abs(x[1] - x[0]) + abs(y[1] - y[0]))
+class Beehive:
+    def __init__(self, num_bees, flowers):
+        self.bees = [Bee() for _ in range(num_bees)]
+        self.generation = 0
+        self.average_scores = []
+        self.flowers = flowers
+
+    def initialize_bees(self):
+        for bee in self.bees:
+            bee.set_genes(self.flowers)
+            bee.calculate_performance()
     
-class Reproduction:
-    def __init__(self):
-        self.parents = []
+    def evolve_generation(self):
+        self.bees.sort(key=lambda bee: bee.performance_score)
+        best_bees = self.bees[:50]
+        new_bees = []
 
-    def set_parents(self, bees_50): 
-        print(len(bees_50))
-        random.shuffle(bees_50)
-        for i in range(0, len(bees_50), 2):
-            if i + 1 < len(bees_50):
-                pair = [bees_50[i], bees_50[i + 1]]
-                self.parents.append(pair)
-        print(len(self.parents))
-
-    def union(self, flowers):
-        parents1 = []
-        parents2 = []
-        for pair in self.parents:
-            parents1.append(pair[0])
-            parents2.append(pair[1])
-
-        x = len(parents1) // 2
-        children1 = parents1[:x] + parents2[x:]
-        children2 = parents2[:x] + parents1[x:]
-        flowers_positions = set(flowers)
-
-        for i in range(len(children1)):
-            if not isinstance(children1[i], Bee):
-                children1[i] = flowers_positions.pop()
-        
-        for i in range(len(children2)):
-            if not isinstance(children2[i], Bee):
-                children2[i] = flowers_positions.pop()
-
-        return children1, children2
-        
+        while len(new_bees) < len(self.bees):
+            parent1, parent2 = random.sample(best_bees, 2)
+            child1, child2 = parent1.reproduce(parent1, parent2)
+            child1.calculate_performance()
+            child2.calculate_performance()
+            new_bees.extend([child1, child2])
+        self.bees = new_bees
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
 import csv
-from beehive import Bee, Reproduction
+import matplotlib.pyplot as plt
+import cProfile
+import pstats
+from beehive import Beehive
+import numpy as np  
 
 def load_flowers(file_path):
     flowers = []
@@ -12,11 +15,57 @@ def load_flowers(file_path):
             flowers.append(flower_tuple)
     return flowers
 
-flowers = load_flowers('flowers.csv')
+def main():
+    num_generations = 100
+    num_bees = 100
+    flowers = load_flowers('flowers.csv')
+    beehive = Beehive(num_bees, flowers)
+    beehive.initialize_bees() 
+    best_bee = None
+    best_score = float('inf')
+    generation_scores = []
+    all_bees_scores = []
 
-# Première génération d'abeilles
-bees = [Bee(f'bee_{i + 1}') for i in range(1, 101)]
-for bee in bees:
-    bee.set_genes(flowers)
-    bee.set_fitness()
+    for i in range(num_generations):
+        beehive.evolve_generation()
+        current_best_bee = min(beehive.bees, key=lambda bee: bee.performance_score)
+        if current_best_bee.performance_score < best_score:
+            best_bee = current_best_bee
+            best_score = current_best_bee.performance_score
+        generation_scores.append(best_score)
+        all_scores = [bee.performance_score for bee in beehive.bees]
+        all_bees_scores.append(all_scores)
 
+    average_scores = [np.mean(scores) for scores in all_bees_scores]
+
+    plt.figure(figsize=(18, 4))
+
+    # parcours de la meilleure abeille
+    plt.subplot(131)
+    best_path = best_bee.genes
+    x, y = zip(*best_path)
+    plt.plot(x, y, marker='o')
+    plt.xlabel('X Coordinate')
+    plt.ylabel('Y Coordinate')
+    plt.title("Best Bee's Path")
+
+    # évolution du score de performance de la meilleure abeille
+    plt.subplot(132)
+    plt.plot(range(num_generations), generation_scores)
+    plt.xlabel('Generation')
+    plt.ylabel("Best Bee's Performance Score")
+    plt.title("Evolution of Best Bee's Performance Score")
+
+    # moyenne du score de performance global (toutes les abeilles)
+    plt.subplot(133)
+    plt.plot(range(num_generations), average_scores, color='green', label='Average Score')
+    plt.xlabel('Generation')
+    plt.ylabel('Average Performance Score')
+    plt.title('Average Performance Score of All Bees')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+    
+if __name__ == '__main__':
+    main()
